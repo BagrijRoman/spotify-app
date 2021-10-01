825924381fad68081f40fe9251da48b794ea597f
roman.bahrii <roman.bahrii@eleks.com>
2021-10-02 00:12:46 +0300

    add get auth token logic

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const querystring = require('querystring');
+const axios = require('axios');
+
+const { generateRandomString } = require('./utils');
+
+const PORT = 8888;
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 const app = express();
 
@@ -6,8 +16,54 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
-const PORT = 8888;
+app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    const scope = 'user-read-private user-read-email';
+
+    const queryParams = querystring.stringify({
+        client_id: SPOTIFY_CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+        state,
+        scope,
+    });
+
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get('/login-callback', (req, res) => {
+    const code = req.query.code || null;
+
+    // todo use async/await instead
+    axios({
+        method: 'POST',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: REDIRECT_URI,
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
+        },
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                res.send(`
+                    <pre>
+                      ${JSON.stringify(response.data, null, 2)}
+                    </pre>
+                `);
+            } else {
+                res.send(response);
+            }
+        })
+        .catch((error) => {
+            res.send(error)
+        })
+});
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
-})
+});
